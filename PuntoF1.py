244516a25127e8306cb74208ddd2b66a8c927c9b
AlejandraCaceres <42219621+AlejandraCaceres@users.noreply.github.com>
2018-10-31 15:59:25 -0500

    Add files via upload
    Graficas de los filtros

--- PuntoF1.py
+++ PuntoF1.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import ifft, fftshift
 from scipy.interpolate import interp1d
-import urllib 
+import urllib2
 
-DD = urllib.request.urlopen("https://raw.githubusercontent.com/AlejandraCaceres/Signal2/master/signal.dat")
-DD2 = urllib.request.urlopen("https://raw.githubusercontent.com/AlejandraCaceres/Incompletos2/master/incompletos.dat")
+DD = urllib2.urlopen("https://raw.githubusercontent.com/AlejandraCaceres/Signal2/master/signal.dat")
+DD2 = urllib2.urlopen("https://raw.githubusercontent.com/AlejandraCaceres/Incompletos2/master/incompletos.dat")
 
 datos_senal = np.genfromtxt(DD, delimiter=',', usecols=(0,1))
 datos_incompletos = np.genfromtxt(DD2, delimiter=',', usecols=(0,1))
@@ -52,4 +46,53 @@ ax.set_xlabel('Frecuencia')
 fig.savefig('CaceresAlejandra_TF.pdf', type='pdf')
 plt.grid()
 print("Frecuencias Principales: Todos los armonicos por encima de 500Hz se interpretan como ruido. En el espectro aparecen 4 picos importantes en 20Hz, 160Hz, 230Hz, 263Hz y 403Hz")
+
+
+
+n = 512
+xmin = min(datos_incompletos[:,0])
+xmax = max(datos_incompletos[:,0])
+
+# X uniforme
+x_completos = np.linspace(xmin, xmax, n)
+
+# y uniforme
+y_completos = np.zeros((n,2))
+y_completos[:,0] = Fq(x_completos)
+y_completos[:,1] = Fc(x_completos)
+
+# datos basicos de la senal
+periodo = x_completos[1]-x_completos[0]
+frecuencia = 1/periodo
+frecuencias_completo = np.linspace(-frecuencia/2, frecuencia/2, n) # vector de frecuencias
+
+
+
+# crear arreglos con todos los valores de k y n
+kk,nn = np.meshgrid(np.arange(n), np.arange(n))
+theta = -2*np.pi*kk*nn/n # Theta es lo que va en la exponencial
+
+transformada_completos = np.zeros((n,2), dtype=complex)
+transformada_completos[:,0] = np.sum(y_completos[:,0]*np.exp(1j*theta) ,1) # hacer las sumas de la transformada
+transformada_completos[:,1] = np.sum(y_completos[:,1]*np.exp(1j*theta) ,1) # hacer las sumas de la transformada
+
+transformada_completos = fftshift(transformada_completos, axes=0) # rotar el arreglo para que coincida con las frecuencias
+
+
+fig, ax = plt.subplots(figsize=(10,6), nrows=3, ncols=1, sharex=True, sharey=True)
+ax[0].plot(frecuencias, np.abs(transformada))
+ax[0].set_yscale('log')
+ax[0].set_xlim([-3000,3000])
+ax[0].grid()
+
+ax[1].plot(frecuencias_completo, np.abs(transformada_completos[:,0]))
+ax[1].set_yscale('log')
+ax[1].grid()
+
+ax[2].plot(frecuencias_completo, np.abs(transformada_completos[:,1]))
+ax[2].set_yscale('log')
+ax[2].set_xlabel('Frecuencia')
+ax[2].grid()
+fig.savefig('CaceresAlejandra_TF_interpola', type='pdf')
+
 
